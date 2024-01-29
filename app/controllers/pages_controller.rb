2f4d996cd036fd5c528425173f4de2bd3513a332
niha-2 <gen.3polym.hn@gmail.com>
2024-01-29 19:28:50 +0900

    [fix] レビュー指摘対応：n+1問題の解消、パーシャルの分割。

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   def index
     return unless user_signed_in?
 
-    @users = User.all
+    @users = User.all.includes(:tweets)
     @tweets = Tweet.all.order(created_at: :desc)
     @followings = Follow.where(follower_id: current_user.id)
     @followings_tweets = Tweet.where(user_id: @followings.pluck(:followed_id)).order(created_at: :desc)
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Tweet < ApplicationRecord
+  belongs_to :user
   validates :tweet, presence: true, length: { maximum: 100 }
 end
