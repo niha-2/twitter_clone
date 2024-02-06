4be4acdea586ce2abed1f87172af762486b30802
niha-2 <gen.3polym.hn@gmail.com>
2024-02-06 21:20:38 +0900

    [fix] rubocopを通した

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,18 @@
-class TweetsController < ApplicationController
+# frozen_string_literal: true
 
+class TweetsController < ApplicationController
   def create
     return unless user_signed_in?
 
     @tweet = Tweet.new(tweet_params)
     @tweet.user = current_user
-    if @tweet.save
-      redirect_to root_path
-    else
-      flash[:alert] = @tweet.errors.full_messages.join(', ')
-      redirect_to root_path
-    end
+    flash[:alert] = @tweet.errors.full_messages.join(', ') unless @tweet.save
+    redirect_to root_path
   end
 
-    private
+  private
 
-    def tweet_params
-      params.require(:tweet).permit(:tweet, :image)
-    end
-
+  def tweet_params
+    params.require(:tweet).permit(:tweet, :image)
+  end
 end
