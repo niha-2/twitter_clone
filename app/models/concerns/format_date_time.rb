08c524d0ab43054bdff28ef127efde25339774e4
niha-2 <gen.3polym.hn@gmail.com>
2024-02-20 23:11:29 +0900

    [fix] レビュー対応：共通処理をモジュールに切り出し、日付のフォーマットを統一する

---
+++ app/models/concerns/format_date_time.rb
@@ -0,0 +1,21 @@
+module FormatDateTime
+  extend ActiveSupport::Concern
+
+  def created_hour_from_now
+    ((Time.zone.now - created_at) / 1.hour).round
+  end
+
+  def created_minute_from_now
+    ((Time.zone.now - created_at) / 1.minute).round
+  end
+
+  def created_period_from_now
+    if created_minute_from_now < 60
+      "#{created_minute_from_now}分前"
+    elsif created_hour_from_now < 24
+      "#{created_hour_from_now}時間前"
+    else
+      I18n.l(created_at, format: :date)
+    end
+  end
+end
