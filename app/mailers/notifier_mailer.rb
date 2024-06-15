581d7cac2404984ec96ac0f0a172c2baf08b4e2d
niha-2 <gen.3polym.hn@gmail.com>
2024-06-15 16:37:51 +0900

    [fix] ヘッダーがメール本文に入ってしまっていたので、メールのレイアウトを変更

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,6 +1,4 @@
 class NotifierMailer < ApplicationMailer
-  default :from => ENV['SENDER_ADDRESS']
-
   def send_notification_email(notice)
     @notice = notice
     mail(to: @notice.tweet.user.email, subject: "Xからの通知です。")
