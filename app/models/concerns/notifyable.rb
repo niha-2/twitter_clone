2322a83a0a27745520fd72b3c6bead04c0cb257f
niha-2 <gen.3polym.hn@gmail.com>
2024-06-25 21:14:59 +0900

    rubocopを通した

--- app/models/concerns/notifyable.rb
+++ app/models/concerns/notifyable.rb
@@ -10,10 +10,9 @@ module Notifyable
   private
 
   def create_and_send_notification
-    notice = Notice.new(user_id: self.user_id, tweet_id: self.tweet_id, action_type: self.class.name.downcase)
+    notice = Notice.new(user_id:, tweet_id:, action_type: self.class.name.downcase)
     return unless notice.save
 
     NotifierMailer.send_notification_email(notice).deliver_now
   end
-
 end
